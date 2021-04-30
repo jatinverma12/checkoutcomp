ecea65c822b718e68c810b7e4a5e3f788372b946
jatin-verma-1999 <jatin71_soe@jnu.ac.in>
2021-04-30 16:40:30 +0530

    fsl

--- index.js
+++ index.js
@@ -31,3 +31,19 @@ const swiper = new Swiper('.swiper-container', {
       el: '.swiper-scrollbar',
     },
   });
+
+  //FLAT 100% COUPON JS
+
+  document.querySelector(".openbtn").style.background="linear-gradient(90deg, rgba(109,66,231,1) 16%, rgba(221,210,252,1) 100%)";
+  
+  function openNav() {
+    document.getElementById("mySidepanel").style.width = "500px";
+    document.querySelector(".fa-caret-up").style.color="#7c5fcf"
+    document.querySelector(".fa-times").style.color="#000"
+    // document.querySelector(".fa-caret-up").style.color="red"
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+    document.querySelector(".fa-caret-up").style.color="#000"
+  }
