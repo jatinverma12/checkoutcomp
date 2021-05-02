c36ca9f33b008db5dbe7b9b1833d2966a1c50896
jatin-verma-1999 <jatin71_soe@jnu.ac.in>
2021-05-02 13:09:38 +0530

    fdsfds

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-var image=document.querySelectorAll(".image img");
-
-image.forEach(i => {
-    i.addEventListener("click",function(evt){
-        var id = this.parentNode.id;
-        alert(id);
-        var div =document.getElementById(`${id}`)
-        var pappu=div.getElementsByClassName('.animation')[0];
-        alert(pappu);
-        pappu.style.display="block";
-      //  anime.style.display="block";
-    });
-    
-    // i.addEventListener("mouseout",()=>{
-    //    // anime.style.display="none";
-    // })
-});
+$( function() {
+  $( "#slider-range" ).slider({
+    range: true,
+    min: 1000,
+    max: 4000,
+    values: [ 1000, 4000 ],
+    slide: function( event, ui ) {
+      $( "#r1" ).val( ui.values[ 0 ]);
+      $("#r2").val(ui.values[1]);
+    }
+  });
+  $( "#r1" ).val( $( "#slider-range" ).slider( "values", 0 ));
+  $( "#r2" ).val( $( "#slider-range" ).slider( "values", 1 ));
+} );
