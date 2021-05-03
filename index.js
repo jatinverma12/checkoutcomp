00cd6779dd35db167838fb574484b6dd50c14040
jatin-verma-1999 <jatin71_soe@jnu.ac.in>
2021-05-03 18:35:46 +0530

    fdsf

--- index.js
+++ index.js
@@ -22,3 +22,52 @@ btn.addEventListener('click',function(){
   else
   filter.style.display="none";
 })
+
+
+var modal3=document.getElementById("myModal3");
+// Get the button that opens the modal
+var btn3=document.getElementById("myBtn3");
+var mdb=document.getElementsByClassName("mobiledropdownbtn")[0];
+
+// Get the <span> element that closes the modal
+// When the user clicks the button, open the modal 
+
+ btn3.onclick = function () {
+   modal3.style.display = "block";
+ }
+
+ mdb.addEventListener("click",function(){
+   if(filter.style.display=="block")
+   filter.style.display="none";
+ })
+ var speed= 10;
+
+ function typeWriter() {
+  if ($(window).width()>768) {
+   btn3.onclick = function () {
+     modal3.style.display = "block";
+   }
+    
+    setTimeout(typeWriter, speed);
+  }
+  else
+  {
+    setTimeout(typeWriter, speed);
+  }
+}
+
+
+setInterval(() => {
+  typeWriter();
+}, 10);
+
+
+
+// When the user clicks on <span> (x), close the modal
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal3) 
+    modal3.style.display = "none";
+}
+
