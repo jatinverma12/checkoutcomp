1f32ea2ecb56de0e037f2da5b5d5918ed4b43684
jatin-verma-1999 <jatin71_soe@jnu.ac.in>
2021-05-04 22:14:35 +0530

    fdsfds

--- index.js
+++ index.js
@@ -1,73 +1,63 @@
-$( function() {
-  $( "#slider-range" ).slider({
-    range: true,
-    min: 1000,
-    max: 4000,
-    values: [ 1000, 4000 ],
-    slide: function( event, ui ) {
-      $( "#r1" ).val( ui.values[ 0 ]);
-      $("#r2").val(ui.values[1]);
-    }
-  });
-  $( "#r1" ).val( $( "#slider-range" ).slider( "values", 0 ));
-  $( "#r2" ).val( $( "#slider-range" ).slider( "values", 1 ));
-} );
 
-var filter=document.getElementsByClassName('filters')[0];
-var btn=document.getElementsByClassName('mobileview')[0];
+$('#myBtn').click(function(){
+    $('.modal1').css("display","block");
+});
 
-btn.addEventListener('click',function(){
-  if(filter.style.display=="none")
-  filter.style.display="block";
-  else
-  filter.style.display="none";
+$('#trackbtn').click(function () {
+    $('.modal11').css("display","block");
+})
+
+$('#helpbtn').click(function () {
+    $('.modal12').css("display","block");
+})
+$('.customercarebtn').click(function () {
+    $('.modal12').css("display","none");
+    $('.modal121').css("display","block");
+})
+$('#cancelbtn').click(function () {
+    $('.modal13').css("display","block");
+})
+
+$('#returnbtn').click(function () {
+    $('.modal14').css("display","block");
+})
+
+$('#exchangebtn').click(function () {
+    $('.modal15').css("display","block");
 })
 
 
-var modal3=document.getElementById("myModal3");
-// Get the button that opens the modal
-var btn3=document.getElementById("myBtn3");
-var mdb=document.getElementsByClassName("mobiledropdownbtn")[0];
-
-// Get the <span> element that closes the modal
-// When the user clicks the button, open the modal 
-
- btn3.onclick = function () {
-   modal3.style.display = "block";
- }
-
- mdb.addEventListener("click",function(){
-   if(filter.style.display=="block")
-   filter.style.display="none";
- })
- var speed= 10;
-
- function typeWriter() {
-  if ($(window).width()>768) {
-   btn3.onclick = function () {
-     modal3.style.display = "block";
-   }
-    
-    setTimeout(typeWriter, speed);
-  }
-  else
-  {
-    setTimeout(typeWriter, speed);
-  }
-}
-
-
-setInterval(() => {
-  typeWriter();
-}, 10);
+$('#filterbtn').click(function () {
+    $('.modal2').css("display","block");
+})
 
 
 
-// When the user clicks on <span> (x), close the modal
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal3) 
-    modal3.style.display = "none";
-}
+
+
+
+
+
+
+
+$(window).click(function(event) {
+    if(event.target.className.split(' ')[0]=='modal1')
+    $('.modal1').css("display","none");
+    else if(event.target.className.split(' ')[0]=='modal11')
+    $('.modal11').css("display","none");
+    else if(event.target.className.split(' ')[0]=='modal12')
+    $('.modal12').css("display","none");
+    else if(event.target.className.split(' ')[0]=='modal121')
+    $('.modal121').css("display","none");
+    else if(event.target.className.split(' ')[0]=='modal13')
+    $('.modal13').css("display","none");
+    else if(event.target.className.split(' ')[0]=='modal14')
+    $('.modal14').css("display","none");
+    else if(event.target.className.split(' ')[0]=='modal15')
+    $('.modal15').css("display","none");
+    else if(event.target.className.split(' ')[0]=='modal2')
+    $('.modal2').css("display","none");
+  })
+
 
